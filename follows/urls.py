e3fd961123d93f8f212512f72f73d4cce5503825
Mathieu <dupycournau@gmail.com>
2021-09-21 18:29:08 +0200

    add search for follow feature

--- follows/urls.py
+++ follows/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = "follows"
 
 urlpatterns = [
-    path('abonnement/', views.AbonnementView.as_view(), name="abonnement"),
-    path('abonnement/<int:pk>/delete/', views.UnfollowView.as_view(), name="unfollow"),
+    path('', views.AbonnementView.as_view(), name="abonnement"),
+    path('<int:pk>/delete/', views.UnfollowView.as_view(), name="unfollow"),
 ]
--- follows/views.py
+++ follows/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
@@ -7,10 +8,25 @@ from follows.models import UserFollows
 class AbonnementView(TemplateView):
     template_name = 'follows/abonnement.html'
 
-    def post(self, request, *args, **kwargs):
+
+    def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context["followed_list"] = UserFollows.objects.filter(followed_user=request.user)
-        context["following_list"] = UserFollows.objects.filter(user=request.user)
+        context["followed_list"] = UserFollows.objects.filter(followed_user=self.request.user)
+        context["following_list"] = UserFollows.objects.filter(user=self.request.user)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        try:
+            founded_user = User.objects.get(username=request.POST['searched-user'])
+            if founded_user != request.user:
+                context['msg'] = f"User {founded_user.username} added to your following list"
+                follow = UserFollows.objects.create(user=request.user, followed_user=founded_user)
+                follow.save()
+            else:
+                context['msg'] = "You can't follow yourself, dumbass"
+        except User.DoesNotExist:
+            context['msg'] = "No user found"
         return render(request, 'follows/abonnement.html', context)
 
 class UnfollowView(TemplateView):
