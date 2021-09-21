b58a661c634e06a7de8ccdc244ad99d396579d69
Mathieu <dupycournau@gmail.com>
2021-09-21 18:16:24 +0200

    add flux and signup view

---
+++ src/views.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+
+class FluxView(TemplateView):
+    template_name = 'website/flux.html'
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = "registration/signup.html"
+    success_url = reverse_lazy('login')
+
+
